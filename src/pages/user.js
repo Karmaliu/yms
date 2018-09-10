87199ce76d2e0b8eaf44066153dd17db03e86d8d
Karmaliu <2134515368@qq.com>
2018-09-10 10:41:13 +0800

    setState

--- src/pages/user.js
+++ src/pages/user.js
@@ -4,13 +4,29 @@ import { Button } from 'antd';
 
 @connect()
 export default class User extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 0,
+        }
+    }
     componentDidMount() {
         this.props.dispatch({
             type: "queryUser",
             payload: { name: "11" },
         })
+        function incrment(state, props) {
+            return { value: state.value + 1 }
+        }
+        this.setState(incrment);
+        this.setState(incrment);
+        this.setState(incrment);
+        this.setState({ value: this.state.value + 1 })
+        this.setState(incrment);
     }
     render() {
+
+        console.log(this.state.value);
         return (
             <div>
                 <Button type="primary">按钮</Button>
